8f00aeec469b941598cc9b533a3a61886c65ef19
JeffCky <jeffcarterky@gmail.com>
2022-08-04 15:21:57 -0400

    updated Detail page to be strongly typed.

--- ComicsGallery/Controllers/ComicBooksController.cs
+++ ComicsGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ComicsGallery.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ComicsGallery.Controllers
 {
@@ -6,18 +7,25 @@ namespace ComicsGallery.Controllers
     {
         public IActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
-            };
-            return View();
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Dan Slott", Role = "Script:" },
+                    new Artist() {Name = "Humberto Ramos", Role = "Pencils:" },
+                    new Artist() {Name = "Victor Olazaba", Role = "Inks:" },
+                    new Artist() {Name = "Edgar Delgado", Role = "Colors:" },
+                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters:" }
+                }
+        };
+
+
+            
+            return View(comicBook);
         }
     }
 }
